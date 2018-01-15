26d4ed23d7744d7e48b84dd8384c0bb09b29b378
Guillaume961 <guillaume.audiberti@gmail.com>
2018-01-15 15:55:36 +0100

    Reponses Exam

--- TP_text_classification/text_classification.py
+++ TP_text_classification/text_classification.py
@@ -8,6 +8,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
 file_list = []
 column_names = ['text', 'category']
@@ -19,12 +20,12 @@ categories = ['ENT','INT','ART','SOC','FRA','SPO','LIV','TEL','UNE']
 
 dataframe = dataframe[dataframe['category'].isin(categories)]
 
-print(dataframe)
+#print(dataframe)
 
 sns.countplot(x='category', data=dataframe, palette="Greens_d")
-plt.show()
+#plt.show()
 
-train, dev, test = np.split(dataframe.sample(frac=1), [int(.6*len(dataframe)), int(.8*len(dataframe))]) # 60%, 20%, 20%
+train, dev, test = np.split(dataframe.sample(frac=1), [int(.6*len(dataframe)), int(.8*len(dataframe))]) # 60%, 20%, 20% # use sklearn.model_selection.train_test_split
 
 vectorizer = CountVectorizer(max_features=1000)
 vectorizer.fit(train['text'])
@@ -32,8 +33,13 @@ X_train_counts = vectorizer.transform(train['text'])
 X_test_counts = vectorizer.transform(test['text'])
 X_dev_counts = vectorizer.transform(dev['text'])
 
-print(X_train_counts);
-print(X_test_counts);
-print(X_dev_counts);
+#print(X_train_counts);
+#print(X_test_counts);
+#print(X_dev_counts);
+
+clf = MultinomialNB()
+clf.fit(X_train_counts, train['category'])
+print(clf.predict(X_test_counts))
+
 
 
